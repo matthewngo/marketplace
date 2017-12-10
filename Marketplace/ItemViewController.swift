df5abe338d363226353b49a6c5a024cf6a0956a4
lantilla <32445340+lantilla@users.noreply.github.com>
2017-12-09 19:18:53 -0800

    some content added to item listing screen

--- Marketplace/ItemViewController.swift
+++ Marketplace/ItemViewController.swift
@@ -11,12 +11,28 @@ import UIKit
 class ItemViewController: UIViewController {
     var itemDescription: NSDictionary = [:]
     
-    @IBOutlet weak var aboutTest: UILabel!
+    @IBOutlet weak var navTitle: UINavigationItem!
+    @IBOutlet weak var fullTitle: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var bestOfferLabel: UILabel!
+    @IBOutlet weak var seller: UILabel!
+    @IBOutlet weak var sellerBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let value = itemDescription["title"] as! String
-        aboutTest.text = value
-        // Do any additional setup after loading the view.
+        let title = itemDescription["title"] as! String
+        navTitle.title = title
+        fullTitle.text = title
+        price.text = "$\(itemDescription["price"] as! String)"
+        let bestOffer = itemDescription["bestOffer"] as! Bool
+        if !bestOffer {
+            bestOfferLabel.isHidden = true
+        } else {
+            bestOfferLabel.isHidden = false
+        }
+        seller.text = itemDescription["email"] as! String
+        sellerBtn.layer.cornerRadius = 7
+        sellerBtn.contentEdgeInsets = UIEdgeInsetsMake(6, 10, 6, 10) // top, left, bottom, right
     }
 
     override func didReceiveMemoryWarning() {
