08f730308be47dd3d3102d225ddf395a91a99a7b
lantilla <32445340+lantilla@users.noreply.github.com>
2017-12-10 17:25:06 -0800

    image on item screen updated

--- Marketplace/ItemViewController.swift
+++ Marketplace/ItemViewController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseStorage
 
 class ItemViewController: UIViewController {
     var itemDescription: NSDictionary = [:]
+    var ref: DatabaseReference?
     
+    @IBOutlet weak var itemImg: UIImageView!
     @IBOutlet weak var navTitle: UINavigationItem!
     @IBOutlet weak var fullTitle: UILabel!
     @IBOutlet weak var price: UILabel!
@@ -20,6 +24,13 @@ class ItemViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let url = itemDescription["imageURL"]  {
+            downloadImage(url: url as! String) // compressing image, still need to fix
+            //itemImg.clipsToBounds = true
+            //itemImg.contentMode = .scaleAspectFill
+        } else {
+            itemImg.image = UIImage(named: "Camera")
+        }
         let title = itemDescription["title"] as! String
         navTitle.title = title
         fullTitle.text = title
@@ -34,7 +45,29 @@ class ItemViewController: UIViewController {
         sellerBtn.layer.cornerRadius = 7
         sellerBtn.contentEdgeInsets = UIEdgeInsetsMake(6, 10, 6, 10) // top, left, bottom, right
     }
-
+    
+    public func downloadImage(url: String) {
+        let imgUrl = URL(string: url)
+        /*
+        URLSession.shared.dataTask(with:imgUrl!, completionHandler: {(data, response, error) in
+            guard let data = data, error == nil else { return }
+            do {
+                self.itemImg.image = UIImage(data: data)
+            } catch let error as NSError {
+                print(error)
+            }
+        }).resume()*/
+        DispatchQueue.main.async {
+            do {
+                let data = try Data(contentsOf: imgUrl!)
+                DispatchQueue.global().sync {
+                    self.itemImg.image = UIImage(data: data)
+                }
+            } catch  {
+                // handle error
+            }
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
