ca40c44d242e03e7e5ededf6e6f690ee65b596c7
lantilla <32445340+lantilla@users.noreply.github.com>
2017-12-10 18:18:07 -0800

    item listing with necessary info

--- Marketplace/ItemViewController.swift
+++ Marketplace/ItemViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 import Firebase
 import FirebaseStorage
 
-class ItemViewController: UIViewController {
+class ItemViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     var itemDescription: NSDictionary = [:]
     var ref: DatabaseReference?
+    var sections = ["Condition", "Description", "Category"]
     
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var itemImg: UIImageView!
     @IBOutlet weak var navTitle: UINavigationItem!
     @IBOutlet weak var fullTitle: UILabel!
@@ -24,7 +26,10 @@ class ItemViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let url = itemDescription["imageURL"]  {
+        tableView.dataSource = self
+        tableView.delegate = self
+        if itemDescription["imageURL"] as! String != ""  {
+            let url = itemDescription["imageURL"]
             downloadImage(url: url as! String) // compressing image, still need to fix
             //itemImg.clipsToBounds = true
             //itemImg.contentMode = .scaleAspectFill
@@ -48,15 +53,6 @@ class ItemViewController: UIViewController {
     
     public func downloadImage(url: String) {
         let imgUrl = URL(string: url)
-        /*
-        URLSession.shared.dataTask(with:imgUrl!, completionHandler: {(data, response, error) in
-            guard let data = data, error == nil else { return }
-            do {
-                self.itemImg.image = UIImage(data: data)
-            } catch let error as NSError {
-                print(error)
-            }
-        }).resume()*/
         DispatchQueue.main.async {
             do {
                 let data = try Data(contentsOf: imgUrl!)
@@ -67,21 +63,61 @@ class ItemViewController: UIViewController {
                 // handle error
             }
         }
+        /*
+         URLSession.shared.dataTask(with:imgUrl!, completionHandler: {(data, response, error) in
+         guard let data = data, error == nil else { return }
+         do {
+         self.itemImg.image = UIImage(data: data)
+         } catch let error as NSError {
+         print(error)
+         }
+         }).resume()*/
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
-    */
-
+    
+    public func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier")
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: "reuseIdentifier")
+        }
+        if indexPath.section == 0 {
+            cell?.textLabel?.text = itemDescription["condition"] as! String
+            if itemDescription["conditionComment"] as! String != ""  {
+                cell?.detailTextLabel?.text = "Comment: \(itemDescription["conditionComment"] as! String)"
+            }
+        } else if indexPath.section == 1 {
+            cell?.textLabel?.text = itemDescription["description"] as! String
+        } else {
+            cell?.textLabel?.text = itemDescription["category"] as! String
+        }
+        
+        return cell!
+    }
+    
+    public func tableView(_ tableView: UITableView,
+                          titleForHeaderInSection section: Int) -> String? {
+        return sections[section]
+    }
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
